5098e0a4d8b440313cc8c201d8ccd4836f7c2ab4
kunal <kunalpopli850@gmail.com>
2021-04-30 13:08:41 +0530

    add

--- read.js
+++ read.js
@@ -1,2 +1,10 @@
 let fs=require("fs");
 let cheerio=require("cheerio")
+
+let content =fs.readFileSync("./q1.html")
+let ch=cheerio.load(content);
+
+//let pkatag=ch("p").text();
+let pkatag=ch("p");
+
+console.log(pkatag);
